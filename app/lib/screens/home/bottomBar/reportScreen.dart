81002e39558e9544234ef5c96d5c3d754965e017
justdvnsh <justdvnsh2208@gmail.com>
2020-06-07 03:24:24 +0530

    added radio boxes

--- app/lib/screens/home/bottomBar/reportScreen.dart
+++ app/lib/screens/home/bottomBar/reportScreen.dart
@@ -28,18 +28,56 @@ class _ReportScreenState extends State<ReportScreen> {
                   Text("Report Your Complaint", style: TextStyle(fontSize: 24),),
                 ],
               ),
-              SizedBox(height: 30),
+              SizedBox(height: 40),
               Padding(
                 padding: const EdgeInsets.only(left: 10),
                 child: Text(
                   "Choose Complaint Identity",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
                     color: Colors.grey[400]
                   ),
                 ),
               ),
+              SizedBox(height: 20),
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 0,
+                  ),
+                  Text("Report on your behalf", style: TextStyle(color: Colors.black, fontSize: 16),),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 1,
+                  ),
+                  Text("Report on someone else's behalf", style: TextStyle(color: Colors.black, fontSize: 16),),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 2,
+                  ),
+                  Text("Report anonymously", style: TextStyle(color: Colors.black, fontSize: 16),),
+                ],
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  "Choose Concerned Department",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[400]
+                  ),
+                ),
+              ),
+              
             ],
           ),
         ),
